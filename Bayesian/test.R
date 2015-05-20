71a0615b6ca0a4363a84953281368215ad98b5a1
bw4sz <benweinstein2010@gmail.com>
2015-05-19 17:40:09 -0700

    added species level plots, still working on interpretation there

--- Bayesian/test.R
+++ Bayesian/test.R
@@ -1,20 +1,10 @@
-y<-sdat[[x]][1:10000,]
-out<-Bayes(presence=y$P_A,distance=y$Phylo.Relatedness,species=y$Species,runs = runsl[x],burn=((runsl[x]/25)-2000),Iteration=y$Iteration,hier=T)
-pars<-melt(out$BUGSoutput$sims.array)
+specSdf$param<-as.factor(str_match(specSdf$parameter,"\\w+"))
+gdf<-group_by(specSdf,Species,param) %>% 
+  summarize(Lower=quantile(estimate,0.025),Upper=quantile(estimate,0.975),mean=mean(estimate))
 
-colnames(pars)<-c("Draw","Chain","parameter","estimate")
+min(gdf$mean)  
 
-
-spec<-pars[str_detect(pars$parameter,"alpha|beta|gamma"), ]
-parmelts<-pars[!str_detect(pars$parameter,"alpha|beta|gamma|deviance"), ]
-
-ggplot(droplevels(parmelts),aes(x=Draw,y=estimate,col=as.factor(Chain))) + geom_line() + facet_wrap(~parameter,scales="free") + theme_bw() + labs(col="Chain")
-
-gdf<-parmelts
-
-trajsplit<-dcast(gdf,...~parameter,value.var="estimate")
-
-#trajsplit<-gdcast[sample(1:nrow(gdcast),500,replace=F),]
+vp<-function(intercept,linear,polynomial,x=seq(0,1,.01)){
 
 #define trajectory function
 trajF<-function(intercept,linear,polynomial,x){
@@ -22,37 +12,19 @@ trajF<-function(intercept,linear,polynomial,x){
   return(p)
 }
 
-specCast<-list()
-
-for(x in 1:length(trajsplit)){
-  a<-trajsplit[[x]]
-  dat<-sdat[[which(names(sdat) %in% names(trajsplit)[x])]]
-  
-  est<-list()
-  for(p in 1:nrow(a)){
-    
-    est[[p]]<-data.frame(Relatedness=dat$Phylo.Relatedness,y=trajF(a[p,]$intercept,a[p,]$linear,a[p,]$polynomial,dat$Phylo.Relatedness))
-    
-  }
-  est<-rbind_all(est)
-  mtraj<-group_by(est,Relatedness) %>% summarise(mean=mean(y),upper=quantile(y,0.975),lower=quantile(y,0.025))
-  
-  specCast[[x]]<-mtraj
+y<-trajF(intercept,linear,polynomial,x)
+d<-data.frame(x=x,y=y)
 }
+d<-vp(-.66,6.7,-12.2)
+ggplot(d,aes(x=x,y=y)) + geom_line()
 
-names(specCast)<-names(trajsplit)
+intercept=-1
+linear=10
+polynomial=-7
 
-#name and reduce
-for(x in 1:length(specCast)){
-  specCast[[x]]$Assemblage<-names(specCast)[x]
-}
+pol<-seq(5,7,.1)
+d<-lapply(pol,function(x){vp(-1,x,-18)})
+names(d)<-pol
+d<-melt(d,id.vars=c("x","y"))
+ggplot(d,aes(x=x,y=y,col=as.numeric(L1))) + geom_line(aes(group=L1)) + scale_color_continuous(low="blue",high="red")
 
-specdf<-rbind_all(specCast)
-
-#define another facet
-#add in observed predicted and eventually simulated labels
-
-specdf[specdf$Assemblage %in% c("Env","Env + Dispersal","Observed"),"Type"]<-"Observed and Predicted"
-
-#overlay together
-ggplot(data=specdf[specdf$Assemblage %in% c("Env","Env + Dispersal","Observed"),],aes(x=Relatedness,y=mean,ymin=lower,ymax=upper,fill=Assemblage)) + geom_ribbon(alpha=.5) + geom_line() + theme_bw()+ labs(x="Phylogenetic distance to closest related species",y="Probability of Occurrence") + scale_fill_manual(values=c("grey80","grey60","black"),labels=c("Environment","Environment + Dispersal","Observed"))  
